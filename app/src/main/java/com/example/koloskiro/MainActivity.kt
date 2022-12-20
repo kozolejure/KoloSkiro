dc5c017479bb150c8f1bff32145f8f7db7682843
kozolejure <77204243+kozolejure@users.noreply.github.com>
2022-12-20 10:15:10 +0100

    Deluje register login TODO

--- app/src/main/java/com/example/koloskiro/MainActivity.kt
+++ app/src/main/java/com/example/koloskiro/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.koloskiro
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-
+private lateinit var auth: FirebaseAuth
 
 
 
@@ -11,6 +19,66 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+        //setup
+        auth = Firebase.auth
+        val registerClicked = findViewById<Button>(R.id.register) as Button
+
+        val usernameText = findViewById<EditText>(R.id.userName) as EditText
+
+
+        val paswordText = findViewById<EditText>(R.id.password) as EditText
+
+
+        //end of setup
+
+        registerClicked.setOnClickListener {
+
+
+
+            val password  = paswordText.text.toString()
+            val email = usernameText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        Toast.makeText(this@MainActivity, "Registracija ", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                       // updateUI(null)
+                    }
+                }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+           // reload();
+        }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
     }
 }
 
