890a96deeeccc50e50541d43b06fe41f2a7547c1
kozolejure <77204243+kozolejure@users.noreply.github.com>
2022-12-21 22:50:44 +0100

    Dodajanje v bazo in registracija dokončana

---
+++ app/src/main/java/com/example/koloskiro/User.kt
@@ -0,0 +1,15 @@
+package com.example.koloskiro
+
+class User(
+    val name : String,
+    val surname : String,
+    val email : String,
+    val address : String,
+    var isProvider: Boolean){
+
+        constructor() : this("test","","","",true){
+    }
+
+
+
+}
