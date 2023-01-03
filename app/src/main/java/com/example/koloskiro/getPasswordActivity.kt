b24a28b844730e6e85a773cf5aba8a8396e81f93
kozolejure <77204243+kozolejure@users.noreply.github.com>
2023-01-03 21:47:53 +0100

    Dodajanje izposoja TODO prejemanje gesla itd

--- app/src/main/java/com/example/koloskiro/getPasswordActivity.kt
+++ app/src/main/java/com/example/koloskiro/getPasswordActivity.kt
@@ -1,17 +1,49 @@
 package com.example.koloskiro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class getPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_password)
 
+        val TextDeviceName = findViewById<TextView>(R.id.TextDevice) as TextView
+        val ProviderEmail = findViewById<TextView>(R.id.TextEmail) as TextView
+
+        val button = findViewById<Button>(R.id.buttonNext) as Button
+
 
 
         var Rent = intent.getSerializableExtra("rentObject") as RentData
+        val db = Firebase.firestore
+
+
+
+        val docRef = db.collection("KoloSkiro").document(Rent.deviceID)
+        docRef.get().addOnSuccessListener { documentSnapshot ->
+            val tempKoloSkiro = documentSnapshot.toObject<KoloSkiro>()
+            if (tempKoloSkiro != null) {
+
+
+
+                ProviderEmail.setText(tempKoloSkiro.owner)
+                TextDeviceName.setText(tempKoloSkiro.type)
+
+
+
+            }
+
+        }
 
 
 
@@ -24,6 +56,27 @@ class getPasswordActivity : AppCompatActivity() {
 
 
         E1.setText(Rent.pin.elementAt(0).toString())
+        E2.setText(Rent.pin.elementAt(1).toString())
+        E3.setText(Rent.pin.elementAt(2).toString())
+        E4.setText(Rent.pin.elementAt(3).toString())
+
+
+
+        button.setOnClickListener(){
+
+
+
+            val intent = Intent(this,CounterActivity::class.java)
+            intent.putExtra("rentObject",Rent)
+            startActivity(intent)
+
+
+
+
+
+
+
+        }
 
 
 
